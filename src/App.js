9e13a7c00ec094e56c0eef96c16f86f01fa0cf86
Mahdi Salari <mac@Mahdis-MacBook-Air.local>
2025-10-10 16:44:03 +0330

    create routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Navbar from './components/navbar';
 import Contacts from './components/contacts/contacts';
 import { useState } from 'react';
+import {Route , Routes , useNavigate, Navigate} from 'react-router-dom'
 import React from 'react'
+import { AddContact, Contact, EditContact } from './components';
 
 export default function App() {
   const [loading , setLoading] = useState(false)
@@ -11,7 +13,14 @@ export default function App() {
   return (
     <div className='App'>
       <Navbar/>
-      <Contacts contacts = {getContacts}  loading={loading}/>
+      <Routes>
+        <Route path='/' element={ <Navigate to="/contacts"/>} />
+        <Route path='/contacts' element={<Contacts contacts={getContacts} loading={loading}/>}/>
+        <Route path='/contacts/add' element={<AddContact/>}/>
+        <Route path='/contacts/:contactId' element={<Contact/>}/>
+        <Route path='/contacts/edit/:contactId' element={<EditContact/>}/>
+      </Routes>
+      {/* <Contacts contacts = {getContacts}  loading={loading}/> */}
     </div>
   )
 }
